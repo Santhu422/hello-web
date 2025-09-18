aca4a79308228545afc75899ad5deb544d0fa8af
Santhu422 <santhukarri1@gmail.com>
2025-09-18 11:14:30 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello Web App</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>hello team! </h1>
+<p> This is managed using GitHub Collaboration </p>
+<script scr="script.js"></script>
+</body>
+</html>
